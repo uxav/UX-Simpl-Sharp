40d429c3f029f50e19678bd8b3ebcb9a2aa34fd8
Mike Jobson <mike@controldesigns.co.uk>
2015-11-01 11:08:45 +0000

    Setup workspace

---
+++ CDSimplSharpPro/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace CDSimplSharpPro
+{
+    public class Class1
+    {
+    }
+}
---
+++ CDSimplSharpPro/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("CDSimplSharpPro")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CDSimplSharpPro")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
