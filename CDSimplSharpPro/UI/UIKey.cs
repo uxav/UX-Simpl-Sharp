f5d24f1dbec0743598246775950baf39eabf6e2b
Mike Jobson <mike@controldesigns.co.uk>
2015-11-12 14:05:09 +0000

    Override for ToString method

--- CDSimplSharpPro/UI/UIKey.cs
+++ CDSimplSharpPro/UI/UIKey.cs
@@ -16,5 +16,10 @@ namespace CDSimplSharpPro.UI
             this.Name = name;
             this.Number = number;
         }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", this.Name, this.Number);
+        }
     }
 }
