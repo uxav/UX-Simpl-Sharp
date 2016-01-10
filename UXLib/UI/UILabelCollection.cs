fd76961ee2fb27014483df2f0140d9ae63426e42
Mike Jobson <mike@controldesigns.co.uk>
2016-01-10 21:03:02 +0000

    Renamed Project

---
+++ UXLib/UI/UILabelCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+namespace UXLib.UI
+{
+    public class UILabelCollection : IEnumerable<UILabel>
+    {
+        private List<UILabel> Labels;
+
+        public UILabel this[uint joinNumber]
+        {
+            get
+            {
+                return this.Labels.FirstOrDefault(b => b.JoinNumber == joinNumber);
+            }
+        }
+
+        public int NumberOfLabels
+        {
+            get
+            {
+                return this.Labels.Count;
+            }
+        }
+
+        public UILabelCollection()
+        {
+            this.Labels = new List<UILabel>();
+        }
+
+        public void Add(UILabel newLabel)
+        {
+            if (!this.Labels.Contains(newLabel))
+            {
+                this.Labels.Add(newLabel);
+            }
+        }
+
+        public IEnumerator<UILabel> GetEnumerator()
+        {
+            return Labels.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ UXLib/UI/UIViewControllerCollection.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace UXLib.UI
+{
+    public class UIViewControllerCollection : IEnumerable<UIViewController>
+    {
+        protected List<UIViewController> ViewControllers;
+        public UITimeOut ViewTimeOut;
+
+        public UIViewController this[uint joinNumber]
+        {
+            get
+            {
+                return this.ViewControllers.FirstOrDefault(p => p.VisibleJoinNumber == joinNumber);
+            }
+        }
+
+        public UIViewController CurrentView
+        {
+            get
+            {
+                return this.ViewControllers.FirstOrDefault(p => p.Visible == true);
+            }
+        }
+
+        public UIViewControllerCollection()
+        {
+            ViewControllers = new List<UIViewController>();
+        }
+
+        public UIViewControllerCollection(UITimeOut timeout)
+        {
+            ViewControllers = new List<UIViewController>();
+            this.ViewTimeOut = timeout;
+        }
+
+        public void Add(UIViewController newView)
+        {
+            ViewControllers.Add(newView);
+            newView.VisibilityChange += new UIViewControllerEventHandler(ViewController_VisibilityChange);
+        }
+
+        public void ShowOnly(UIViewController newView)
+        {
+            foreach (UIViewController view in ViewControllers)
+            {
+                if (view != newView)
+                {
+                    view.Hide();
+                }
+            }
+
+            if (ViewControllers.Contains(newView))
+            {
+                newView.Show();
+            }
+        }
+
+        public UIViewController GetCurrentView()
+        {
+            return ViewControllers.FirstOrDefault(v => v.Visible);
+        }
+
+        void ViewController_VisibilityChange(UIViewController sender, UIViewVisibilityEventArgs args)
+        {
+            if (sender.Visible && this.ViewTimeOut != null)
+            {
+                this.ViewTimeOut.Set();
+            }
+        }
+
+        public IEnumerator<UIViewController> GetEnumerator()
+        {
+            return this.ViewControllers.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public virtual void Dispose()
+        {
+            this.ViewTimeOut.Dispose();
+
+            foreach (UIViewController view in ViewControllers)
+            {
+                view.VisibilityChange -= new UIViewControllerEventHandler(ViewController_VisibilityChange);
+                view.Dispose();
+            }
+
+            this.ViewControllers.Clear();
+            this.ViewControllers = null;
+        }
+    }
+}
