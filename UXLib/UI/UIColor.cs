7c15d1f8e5aa4a3d026ef7243669ed0264a9527f
Mike Jobson <mike@ux.digital>
2016-03-09 17:51:29 +0000

    Added classes to deal with UI text attributes

---
+++ UXLib/UI/UIColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace UXLib.UI
+{
+    public class UIColor
+    {
+        public UIColor()
+            : this(0, 0, 0) { }
+
+        public UIColor(uint red, uint green, uint blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public UIColor(string hex)
+        {
+            hex = hex.Replace("#", "");
+            if (hex.Length != 6)
+                throw new Exception("Hex color code is not correct length");
+            Red = uint.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            Green = uint.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            Blue = uint.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+
+        public string ToHex()
+        {
+            return "#" + Red.ToString("X2") + Green.ToString("X2") + Blue.ToString("X2");
+        }
+
+        public uint Red;
+        public uint Green;
+        public uint Blue;
+
+    }
+}
---
+++ UXLib/UI/UIText.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.CrestronXml;
+using Crestron.SimplSharp.CrestronXmlLinq;
+
+namespace UXLib.UI
+{
+    public class UIText
+    {
+        public UIText(string fromText)
+        {
+            xml = new XDocument(new XElement("FONT", fromText));
+        }
+
+        public UIText(UIText item)
+        {
+            xml = XDocument.Load(new XmlReader(item.ToString()));
+        }
+
+        XDocument xml;
+
+        public override string ToString()
+        {
+            StringWriter sw = new StringWriter();
+            XElement element = xml.Element("FONT");
+            element.WriteTo(new XmlTextWriter(sw));
+            return sw.ToString();
+        }
+
+        public UIText Color(UIColor color)
+        {
+            XElement element = xml.Element("FONT");
+            element.SetAttributeValue("color", color.ToHex());
+            return this;
+        }
+
+        public UIText Color(uint red, uint green, uint blue)
+        {
+            return Color(new UIColor(red, green, blue));
+        }
+
+        public UIText Color(string hexValue)
+        {
+            return Color(new UIColor(hexValue));
+        }
+
+        public UIText Color()
+        {
+            return Color(new UIColor());
+        }
+
+        public UIText Face(string faceName)
+        {
+            XElement element = xml.Element("FONT");
+            element.SetAttributeValue("face", faceName);
+            return this;
+        }
+
+        public UIText Size(uint size)
+        {
+            XElement element = xml.Element("FONT");
+            element.SetAttributeValue("size", size);
+            return this;
+        }
+    }
+}
