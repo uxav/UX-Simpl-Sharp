f123ffcff3d0cbee719a347ea118e74c0bcc26f7
Mike Jobson <Mike Jobson>
2016-04-01 13:46:52 +0100

    Added extension method for string class

---
+++ UXLib/Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Crestron.SimplSharp;
+
+namespace UXLib.Extensions
+{
+    public static class StringExtension
+    {
+        public static string SplitCamelCase(this string s)
+        {
+            return Regex.Replace(Regex.Replace(s, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+        }
+    }
+}
