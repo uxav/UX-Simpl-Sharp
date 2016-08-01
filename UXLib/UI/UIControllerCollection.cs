13add5845a7e138dadfdc689ffac1714ad73a849
Mike Jobson <mike@ux.digital>
2016-08-01 19:54:08 +0100

    Prevent duplicate id's

--- UXLib/UI/UIControllerCollection.cs
+++ UXLib/UI/UIControllerCollection.cs
@@ -9,39 +9,48 @@ namespace UXLib.UI
 {
     public class UIControllerCollection : IEnumerable<UIController>
     {
-        private List<UIController> interfaces;
+        private Dictionary<uint, UIController> interfaces;
 
         public UIController this[uint id]
         {
             get
             {
-                return this.interfaces.FirstOrDefault(i => i.ID == id);
+                return this.interfaces[id];
             }
         }
 
         public UIControllerCollection()
         {
-            this.interfaces = new List<UIController>();
+            this.interfaces = new Dictionary<uint, UIController>();
         }
 
         protected UIControllerCollection(List<UIController> fromList)
         {
-            interfaces = new List<UIController>(fromList);
+            interfaces = new Dictionary<uint, UIController>();
+            foreach (UIController ui in fromList)
+            {
+                interfaces[ui.ID] = ui;
+            }
         }
 
         public void Add(UIController ui)
         {
-            this.interfaces.Add(ui);
+            this.interfaces[ui.ID] = ui;
+        }
+
+        public bool IsDefined(uint id)
+        {
+            return interfaces.ContainsKey(id);
         }
 
         public UIControllerCollection ForRoom(Models.Room room)
         {
-            return new UIControllerCollection(interfaces.Where(ui => ui.Room == room).ToList());
+            return new UIControllerCollection(interfaces.Values.Where(ui => ui.Room == room).ToList());
         }
 
         public IEnumerator<UIController> GetEnumerator()
         {
-            return this.interfaces.GetEnumerator();
+            return this.interfaces.Values.GetEnumerator();
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
