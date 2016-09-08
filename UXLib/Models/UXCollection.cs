d545ec2173254be65af126e82d01e409aee8ad03
Mike Jobson <mike@ux.digital>
2016-09-08 12:45:36 +0100

    Added collection models

---
+++ UXLib/Models/UXCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace UXLib.Models
+{
+    /// <summary>
+    /// A read only collection of a type
+    /// </summary>
+    /// <typeparam name="T">Type of object stored with a uint key value</typeparam>
+    public class UXCollection<T> : UXReadOnlyCollection<uint, T>, IEnumerable<T>, IEnumerable
+    {
+        internal UXCollection() { }
+
+        public override T this[uint key]
+        {
+            get
+            {
+                return base[key];
+            }
+            internal set
+            {
+                base[key] = value;
+            }
+        }
+
+        internal override void Add(uint key, T value)
+        {
+            base.Add(key, value);
+        }
+
+        public override int IndexOf(T value)
+        {
+            return base.IndexOf(value);
+        }
+
+        public List<T> ToList()
+        {
+            return InternalDictionary.Values.ToList();
+        } 
+
+        #region IEnumerable<T> Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InternalDictionary.Values.GetEnumerator();
+        }
+
+        #endregion
+    } 
+}
---
+++ UXLib/Models/UXReadOnlyCollection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+
+namespace UXLib.Models
+{
+    public class UXReadOnlyCollection<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+    {
+        internal UXReadOnlyCollection()
+        {
+            InternalDictionary = new Dictionary<TKey, TValue>();
+        }
+        
+        protected Dictionary<TKey, TValue> InternalDictionary;
+
+        public virtual TValue this[TKey key] {
+            get
+            {
+                return InternalDictionary[key];
+            }
+            internal set
+            {
+                InternalDictionary[key] = value;
+            }
+        }
+
+        internal virtual void Add(TKey key, TValue value)
+        {
+            InternalDictionary.Add(key, value);
+        }
+
+        public virtual bool Contains(TKey key)
+        {
+            return InternalDictionary.ContainsKey(key);
+        }
+
+        public virtual bool Contains(TValue value)
+        {
+            return InternalDictionary.Values.Contains(value);
+        }
+
+        public virtual int IndexOf(TValue value)
+        {
+            return InternalDictionary.Values.ToList().IndexOf(value);
+        }
+
+        public int Count { get { return InternalDictionary.Count; } }
+
+        #region IEnumerable<KeyValuePair<TKey,TValue>> Members
+
+        public virtual IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return InternalDictionary.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
